c53d3a8ea66e87b124cbc779a616f5083883dda1
Aslan <47611260+MadBro1@users.noreply.github.com>
2019-03-07 11:43:00 -0800

    Create Juventus, right-foot players.py

---
+++ Juventus, right-foot players.py
@@ -0,0 +1,11 @@
+import csv
+with open('data.csv',encoding="utf8") as f:
+    reader = csv.reader(f)
+
+    next(reader)
+
+    data = []
+
+    for row in reader:
+        if row[5] == "Juventus" and row[8] == "Right":
+            print(row[2])
